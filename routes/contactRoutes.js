30791dfd4a7913a3cc649ff0a771cc7be20a3dd4
mahmud-alam <official.mahmud23@gmail.com>
2025-03-05 14:57:01 +0600

    create post, put delete req

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Get all contact" });
+  res.status(200).json({ message: "Get all Contacts" });
+});
+
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get Contact for ${req.params.id}` });
+});
+
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "Create Contact" });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
 });
 
 module.exports = router;
