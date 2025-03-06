5638f02ddd62cd4131d2814a57a28a28db7c011e
Mahmud-Alam <official.mahmud23@gmail.com>
2025-03-06 23:31:03 +0600

    create all error status code handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,21 +1,43 @@
+const { constants } = require("../constants");
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500;
-    switch (statusCode) {
-        case 400:
-            res.json({
-                title: "Validation Failed!",
-                message: err.message,
-                stackTrace: err.stack
-            });
-            break;
-        case 404:
-            res.json({
-                title: "Not Found",
-                message: err.message,
-                stackTrace: err.stack
-            });
-            break;
-    }
-}
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case constants.BAD_REQUEST:
+      res.json({
+        title: "400 - Bad Request",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "401 - Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.FORBIDDEN:
+      res.json({
+        title: "403 - Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.NOT_FOUND:
+      res.json({
+        title: "404 - Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "500 - Internal Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    default:
+      console.log("No Error Found");
+      break;
+  }
+};
 
 module.exports = errorHandler;
