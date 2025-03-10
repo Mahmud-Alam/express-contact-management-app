73b708398e2ad17e0748ba42365496dadffb1719
mahmud-alam <official.mahmud23@gmail.com>
2025-03-10 12:55:42 +0600

    create login user endpoint and generate access token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 // @desc Register a user
@@ -42,7 +43,35 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login the user!" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const user = await User.findOne({ email });
+
+  //compare password with hased password
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (user && comparePassword) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or Password is Incorrect!");
+  }
 });
 
 // @desc Current user info
