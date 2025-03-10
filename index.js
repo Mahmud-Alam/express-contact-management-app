da701e182dc1c9acd83cd9b77b879e9509af9ea9
mahmud-alam <official.mahmud23@gmail.com>
2025-03-10 09:31:15 +0600

    create routes for users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const router = require("./routes/contactRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDB = require("./config/dbConnection");
 
@@ -9,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/api/contacts', router);
+app.use('/api/contacts', contactRoutes);
+app.use('/api/users', userRoutes);
 app.use(errorHandler);
 
 app.listen(port, ()=>{
